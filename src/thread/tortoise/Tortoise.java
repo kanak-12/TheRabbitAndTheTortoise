a09851c4a9566dd0c37b7a91188ae0f8df7233f4
Kanak-12 <kanak.srivastava_cs19@gla.ac.in>
2020-10-29 21:39:32 +0530

    add a constructor in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -7,4 +7,9 @@ public class Tortoise implements Runnable {
     private static final int MILESTONES = 5;
 
     private final Thread tortoise;
+
+    public Tortoise() {
+        this.tortoise = new Thread(this, "Tortoise");
+    }
 }
+
