ebac23ba1979a8304b95046764b237e030698eca
Kanak-12 <kanak.srivastava_cs19@gla.ac.in>
2020-10-29 21:36:52 +0530

    add private Thread in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,6 +1,10 @@
 package thread.tortoise;
 
 import javax.swing.*;
+
 public class Tortoise implements Runnable {
+
     private static final int MILESTONES = 5;
+
+    private final Thread tortoise;
 }
